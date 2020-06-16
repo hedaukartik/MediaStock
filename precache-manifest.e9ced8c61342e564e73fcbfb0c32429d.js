c0c618b029d1b605e886a91385c1518ab513d4e4
hedaukartik <hedaukartik2009@gmail.com>
2020-06-16 12:53:58 +0530

    Updates

---
+++ precache-manifest.e9ced8c61342e564e73fcbfb0c32429d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d57321949fc197c0b9e5c6b8b363d08",
+    "url": "/MediaStock/index.html"
+  },
+  {
+    "revision": "d6daeb3de0dafbe69453",
+    "url": "/MediaStock/static/css/2.7ef9b196.chunk.css"
+  },
+  {
+    "revision": "030947c11632a7392df8",
+    "url": "/MediaStock/static/css/main.5d327a65.chunk.css"
+  },
+  {
+    "revision": "d6daeb3de0dafbe69453",
+    "url": "/MediaStock/static/js/2.a9ddcfd8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MediaStock/static/js/2.a9ddcfd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030947c11632a7392df8",
+    "url": "/MediaStock/static/js/main.5bd252b2.chunk.js"
+  },
+  {
+    "revision": "f32c83d09b7b413ad71b",
+    "url": "/MediaStock/static/js/runtime-main.a9ee559d.js"
+  }
+]);
